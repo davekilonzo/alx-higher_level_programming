b6e5bbc6755395793989ab319ddacd06e4e14c0c
davekilonzo <davidkilonzo1998@gmail.com>
2024-03-13 12:37:26 -0700

    6-max_integer_test.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-    this function adds 2 numbers
-    if the numbers arent int or float
-    raise a TypeError
-    otherwise, return the sum
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """ add two integers or floats """
-    if type(a) is not int and type(a) is not float:
+    """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        if a == float('inf') or a == float('-inf'):
-            raise OverflowError("cannot convert float infinity to integer")
-        a = int(a)
-    if type(b) is float:
-        if b == float('inf') or b == float('-inf'):
-            raise OverflowError("cannot convert float infinity to integer")
-        b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
