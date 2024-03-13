14a8f163ca9edd81f1cdefe80d9e4fc7df19c3c5
davekilonzo <davidkilonzo1998@gmail.com>
2024-03-13 08:38:18 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,7 +14,11 @@ def add_integer(a, b=98):
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     if type(a) is float:
+        if a == float('inf') or a == float('-inf'):
+            raise OverflowError("cannot convert float infinity to integer")
         a = int(a)
     if type(b) is float:
+        if b == float('inf') or b == float('-inf'):
+            raise OverflowError("cannot convert float infinity to integer")
         b = int(b)
     return (a + b)
