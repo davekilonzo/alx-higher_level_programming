4faa1e6d888bf1cb0b15f34496ff69fd80d1dca6
davekilonzo <davidkilonzo1998@gmail.com>
2024-03-13 08:24:48 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
+"""
+    this function adds 2 numbers
+    if the numbers arent int or float
+    raise a TypeError
+    otherwise, return the sum
+"""
+
+
 def add_integer(a, b=98):
-    """
-    add 2 integers.
-
-    Prototype: def add_integers(a, b=98)
-    a and b must be integers or floats
-    if not, raise a TypeError exception
-    Return: returns the sum of a and b
-    """
-    try:
-        a = int(a)
-    except (ValueError, TypeError):
+    """ add two integers or floats """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-
-    try:
-        b = int(b)
-    except(ValueError, TypeError):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-
-    return a + b
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return (a + b)
