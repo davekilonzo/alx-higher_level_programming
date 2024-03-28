4438d10e87467285489be9a881b3b214698f0823
davekilonzo <davidkilonzo1998@gmail.com>
2024-03-28 06:57:37 -0700

    corrections

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,58 +1,26 @@
-#!/usr/bin/python3
-
 import sys
 
-def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column up to the current row
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-    
-    # Check upper left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    
-    # Check upper right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == 1:
-            return False
-            
-    return True
 
-def solve_nqueens(board, row, N, result):
-    if row == N:
-        result.append(["".join(map(str, row)) for row in board])
-        return
-    
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row][col] = 1
-            solve_nqueens(board, row + 1, N, result)
-            board[row][col] = 0
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's.
+    Args:
+        n: integer to initialize
 
-def nqueens(N):
-    if not N.isdigit():
-        print("N must be a number")
-        sys.exit(1)
+    Return: list of chessboard
+    """
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
 
-    N = int(N)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    
-    board = [[0] * N for _ in range(N)]
-    result = []
-    solve_nqueens(board, 0, N, result)
-    
-    for solution in result:
-        print("[{}]".format(", ".join(solution)))
-    
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    
-    nqueens(sys.argv[1])
+def board_deepcopy(board):
+    """Creates a deepcopy of a chessboard.
+    Arg:
+        board: chessboard to make copy from.
+
+    Return: a deepcopy of a chessboard.
+    """
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
 
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,116 +1,86 @@
 #!/usr/bin/python3
-""" Write a class Rectangle that defines a rectangle
-"""
+
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """creates class that defines a rectangle
+    """Represent a rectangle.
     Attributes:
-    empty
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
     """
 
-    def __init__(self, width=0, height=0):
-        """ method to initialize a Rectangle
+    number_of_instances = 0
+    print_symbol = "#"
 
-        Attribute:
-            width (int): optional, 0 by default
-            height (int): optional, 0 by default
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        self.__height = height
-        self.__width = width
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """
-        gets width
-
-        Returns:
-            width (int)
-        """
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        sets width of the rectangle
-
-        Attributes:
-            width (int)
-
-        Raises:
-            TypeError: width is not an integer
-            ValueError: width is negative
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """
-        gets height
-        Returns:
-            height (int)
-        """
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        sets height of the rectangle
-
-        Attributes:
-            height (int)
-
-        Raises:
-            TypeError: height is not an integer
-            ValueError: height is negative
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
-        """Public instance method that returns the rectangle area
-
-        Attributes:
-            area (int)
-
-        Returns:
-            area (int): area of the rectangle
-        """
-        return self.__height * self.__width
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """Public instance method that returns the rectangle perimeter
-        Attributes:
-            perimeter (int): perimeter of the rectangle
-
-        Returns:
-            perimeter (int): 0 if height or width = 0
-        """
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        else:
-            return 2 * self.__height + 2 * self.__width
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """prints a rectangle with #
-
-        Returns:
-            a rectangle with #
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-            return ""
-        else:
-            rectangle_str = ""
-            for i in range(self.__height):
-                rectangle_str += "#" * self.__width + "\n"
-            return rectangle_str
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
+
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
 
