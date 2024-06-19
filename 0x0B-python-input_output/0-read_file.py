5f1205b695e7ad6f81f1f17f513e87bd9bc217f9
davekilonzo <davidkilonzo1998@gmail.com>
2024-06-19 01:50:11 -0700

    first

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
-#!/usr/bin/python3
-
+#!/bin/python3
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    try:
+        with open(filename, "r", encoding="UTF-8") as f:
+            content = f.read()
+            if content:
+                print(content, end="")
+            else:
+                print("File is empty.")
+    except FileNotFoundError:
+        print("File not found.")
+
